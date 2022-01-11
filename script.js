85dff4e3e420078cfe70d50d3e393fd645eeaad2
Urja Arora <urjaarora5@gmail.com>
2022-01-11 20:27:01 +0530

    feat: integrate API

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-const API_KEY = "ef0022da8734e11c7fa3b83e4238b9be";
-const name = "naruto";
-const URL = `https://animechan.vercel.app/api/quotes/anime?title=${name}`;
+function displayWeather(data) {
+  const { name } = data;
+  const { temp, humidity } = data.main;
+  const { icon, description } = data.weather[0];
+  const { speed } = data.wind;
 
-console.log("start");
-fetch(URL)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-console.log("end");
+  document.querySelector(".location").innerText = `Weather in ${name}`;
+  document.querySelector(".temp").innerText = `${temp}°C`;
+  document.querySelector(
+    ".icon"
+  ).src = `https://openweathermap.org/img/wn/${icon}.png`;
+  document.querySelector(".climate").innerText = `${description}`;
+  document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+  document.querySelector(".wind").innerText = `Wind speed: ${speed} km/h`;
+
+  const URL = `https://source.unsplash.com/1600x1080/?${name}`;
+  document.body.style.backgroundImage = `url(${URL})`;
+}
+
+function fetchWeather(cityName) {
+  const API_KEY = "ef0022da8734e11c7fa3b83e4238b9be";
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`;
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => displayWeather(data));
+}
+
+function search() {
+  const cityName = document.querySelector(".input").value;
+  fetchWeather(cityName);
+}
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  search();
+});
+
+document.querySelector(".input").addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    search();
+  }
+});
